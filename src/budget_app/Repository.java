adf34f5c71e1f9361f405aa3323ccdecb06e9597
HaileB65 <brendon.haile@gmail.com>
2022-05-25 21:14:58 -0500

    Updating repository method

--- src/budget_app/Repository.java
+++ src/budget_app/Repository.java
@@ -13,40 +13,7 @@ class Repository {
                 // This will load the MySQL driver, each DB has its own driver
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
-             /* Other examples include:
-                 Class.forname("oracle.jdbc.driver.oracledriver");
-                 Class.forName("org.postgresql.Driver");
-                 Class.forName("mongodb.jdbc.mongodriver"); */
-
-
-                // configure the "connection string" (below) with the following format
-                // -------------------------------------
-                // jdbc:/mysql://H.O.S.T:<PORT>/DATABASE
-                // -------------------------------------
-                // on our localhost we don't need the port ":3306", we will also
-                // add the login credentials and one or more other arguments
-                // within the connection String. Your username here is most
-                // likely "root", unless you have already created
-                // new database users. The default MySQL username is "root".
-
-
-                // NOTE: depending on your version of MySQL, you may need to delete "&useSSL=false"
-                // from the connection String below in order to connect to the database
-
-
-//                String connectionString = "jdbc:mysql://localhost:3306/mybudgetdb?"  connection string
-//                        + "user=root&password=fdal87439KJIOD@#$^";
-//                String connectionString = "jdbc:mysql://root:fdal87439KJIOD@#$^@localhost:3306/mybudgetdb";
-//                        + "user=root&password=fdal87439KJIOD@#$^";
-//                        + "&useSSL=false&allowPublicKeyRetrieval=true";
-
-
-                // Example connection String to remote MySQL instance:
-                // "jdbc:mysql://178.34.21.99:3306/codingnomads?user=root&password=3tr0ngP@33W0rd"
-
-
                 // Setup the connection with the DB
-//                connection = DriverManager.getConnection(connectionString);
                 connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/mybudgetdb","root","fdal87439KJIOD@#$^"); // working connection string
 
 
