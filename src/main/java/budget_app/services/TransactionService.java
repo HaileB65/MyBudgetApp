4a6b6727c6fe2bdb2097bea7c08f4060bbdf8b97
HaileB65 <brendon.haile@gmail.com>
2022-11-03 13:04:35 -0500

    Added better Budget App Pic 1 file

--- src/main/java/budget_app/services/TransactionService.java
+++ src/main/java/budget_app/services/TransactionService.java
@@ -11,7 +11,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class TransactionService {
